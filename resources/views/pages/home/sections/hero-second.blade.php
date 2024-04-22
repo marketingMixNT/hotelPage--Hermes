6ccc5f01afb6144351101ddac5211c16e8f77499
Marek <marekgacek45@gmail.com>
2024-04-22 14:43:48 +0200

    update hero-second

--- resources/views/pages/home/sections/hero-second.blade.php
+++ resources/views/pages/home/sections/hero-second.blade.php
@@ -1,8 +1,21 @@
-<section class="px-40  py-40  h-[80vh] bg-[url('/public/assets/images/hero2.jpeg')] bg-cover bg-center bg-fixed relative bg-gray-300 bg-blend-multiply">
+<section
+    class="px-10 xl:px-20 2xl:px-40  py-10  h-[90vh] 
+    {{-- bg-bg-[url('/public/assets/images/hero2--mobile.jpeg')] 
+    md:bg-[url('/public/assets/images/hero2--desktop.jpeg')] 
+    2xl:bg-[url('/public/assets/images/hero2.jpeg')]  --}}
+    bg-[url('/public/assets/images/hero2.jpeg')] 
+
+    bg-cover bg-center bg-fixed relative bg-gray-300 bg-blend-multiply flex justify-end items-end">
 
 
-    <div class="absolute bottom-16 left-40 right-40 inline"><p class="text-3xl text-fontWhite font-light font-text leading-normal tracking-wide"> <span class="uppercase font-normal text-base mr-8">NASZE WIDOKI
-        </span>  <span class="font-normal">ZANURZ SIĘ W NIEZAKŁÓCONEJ KRASIE GÓRSKIEGO KRAJOBRAZU</span>
-       Górski Azyl położony jest w samym sercu majestatycznych gór, gdzie każde okno i każdy kąt oferuje zapierające dech w piersiach widoki. Ciesz się spokojem i pięknem rozległych lasów, szumiących strumieni i górskich szczytów, które otaczają nasz domek. </p>
+    <div class="">
+        <p class="xs:text-xl md:text-2xl xl:text-3xl text-fontWhite font-light font-text leading-normal tracking-wide"> <span
+                class="uppercase font-normal text-base mr-8">NASZE WIDOKI
+            </span> <span class="font-normal">ZANURZ SIĘ W NIEZAKŁÓCONEJ KRASIE GÓRSKIEGO KRAJOBRAZU</span>
+            Hotel Królewski Szczyt, położony w sercu Tatr, oferuje niezapomniane widoki na górskie szczyty i zielone
+            doliny, zapewniając idealne warunki do relaksu w harmonii z naturą.</p>
     </div>
 </section>
+
+
+{{-- xs:text-xl md:text-2xl xl:text-3xl --}}
