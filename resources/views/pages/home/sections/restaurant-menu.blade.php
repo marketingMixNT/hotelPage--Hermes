a75638f5b2a28bc79babf4312f54a0e4facface0
Marek <marekgacek45@gmail.com>
2024-04-23 11:34:01 +0200

    finissh restaurant section

--- resources/views/pages/home/sections/restaurant-menu.blade.php
+++ resources/views/pages/home/sections/restaurant-menu.blade.php
@@ -1,54 +1,83 @@
-<section class="px-40 pt-40 pb-20 bg-bgThird ">
+<section class="px-10 xl:px-20 2xl:px-40  bg-bgThird">
+    <x-grid>
 
-    <div class="grid grid-cols-12 ">
-        <img src="{{asset('assets/images/1.jpeg')}}" class="col-start-1 col-end-6 rounded-lg max-h-[1000px] w-full object-cover"></img>
-     <p class="col-start-9 col-end-13 font-headingSecondary font-light text-fontPrimary text-4xl mt-12 leading-snug tracking-wide"><span class="font-normal">  PRZYTULNE SCHRONIENIE W GÓRACH – WITAJ W GÓRSKIM AZYLU, GDZIE NATURA KOI DUSZĘ.</span>
-         Miłośnicy natury i spokoju znajdą tu swoje sanktuarium, gdzie tradycja miesza się z nowoczesnością w harmonijnym odosobnieniu. <span class="font-normal"> Górski Azyl</span>, położony w malowniczym zakątku gór, to miejsce, gdzie można na nowo połączyć się z przyrodą, zainspirować się i nawiązać trwałe więzi.</p>
- 
-     
-     <img src="{{asset('assets/images/2.jpeg')}}" class="col-start-7 col-end-13 rounded-lg min-h-[600px] w-full object-cover -mt-32"></img>
-   
- 
- </div>
- 
- 
- </section>
+      <div class="xl:col-start-1 xl:col-end-6  sm:pr-20 md:pr-64 xl:pr-0 max-h-[500px] md:max-h-[800px] xl:max-h-[1000px] mb-10 sm:mb-20 xl:mb-0">
 
- <section class="px-40 pb-40 bg-bgThird ">
- 
-    <div class="grid grid-cols-12 ">
-        {{-- <img src="{{asset('assets/images/4.jpeg')}}" class="col-start-1 col-end-8 rounded-lg min-h-[600px] w-full object-cover "></img> --}}
-
-
-        <div
-        class="menu-box border border-primary-400   relative px-4    col-start-1 col-end-6 mx-16  flex flex-col gap-6 justify-center items-center py-12 -mt-32">
-      <div class="flex flex-col justify-center items-center gap-1">
-        <span class="uppercase  text-fontPrimary">Breakfast</span>
-        <span class="uppercase  text-fontPrimary">pon - nd <span>07:30 - 10:00</span></span>
-      </div>
-      <div class="flex flex-col justify-center items-center gap-1">
-        <span class="uppercase  text-fontPrimary">Breakfast</span>
-        <span class="uppercase  text-fontPrimary">pon - nd <span>07:30 - 10:00</span></span>
-      </div>
-      <div class="flex flex-col justify-center items-center gap-1">
-        <span class="uppercase  text-fontPrimary">Breakfast</span>
-        <span class="uppercase  text-fontPrimary">pon - nd <span>07:30 - 10:00</span></span>
+        <img src="{{ asset('assets/images/6.jpeg') }}" alt="danie w naszej restauracji"
+        class=" rounded-lg  w-full h-full object-cover"></img>
       </div>
 
-    </div>
 
+        <div class="xl:col-start-9 xl:col-end-13 flex flex-col  items-start gap-8  sm:pl-20 md:pl-64 xl:pl-0 mb-10 sm:mb-20 xl:mb-0">
 
-
-        <div class="col-start-7 col-end-13 mt-24  flex flex-col gap-4 ">
-{{-- <h2 class=" font-headingSecondary  text-fontPrimary text-4xl  leading-snug tracking-wide">Ciesz się Naturą i Komfortem w Górskim Azylu</h2> --}}
-            <p class="font-headingSecondary  text-fontPrimary text-3xl  leading-snug tracking-wide">
-               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores dolores quidem enim inventore rem, corrupti quia distinctio aliquam necessitatibus, eveniet quae? Blanditiis, fugit dolorum!
+            <h2 class="heading mr-12">Smakowite Chwile i Magiczne Wieczory w Tatry Grand Hotel</h2>
+            <p class="text">
+                Tatry Grand Hotel w Zakopanem to <span class="uppercase font-semibold">perła polskiej hotelarstwa</span>,
+                oferująca luksus i relaks w sercu Tatr. Elegancja pięciogwiazdkowego zakwaterowania łączy się z ciepłem
+                zakopiańskiej tradycji. Od wyrafinowanych wnętrz, przez ofertę spa, po widoki na góry – wszystko zostało
+                dopracowane, by zapewnić niezapomniane wrażenia i komfort.
             </p>
-            <span class="uppercase text-primary-400">Marian, google / 2023-11-30</span>
-            {{-- <p class=" font-text font-light text-fontPrimary text leading-normal tracking-wide">
-                Podążaj ścieżkami prowadzącymi do ukrytych zakątków naszego ogrodu, gdzie możesz cieszyć się ciszą lub znaleźć inspirację w otoczeniu dzikiej przyrody. Nasz taras widokowy oferuje niezapomniane panoramy górskie, idealne do kontemplacji lub porannych sesji jogi. W Górskim Azylu łączymy lokalne tradycje z nowoczesnym komfortem, tworząc przestrzeń, w której można naładować baterie i nawiązać nowe przyjaźnie.
-            </p> --}}
+            <a href="#" class="link-dotted">Dowiedz się więcej</a>
+            <div class="border-t border-b border-primary-400 w-full  sm:py-3 mt-6 flex justify-between items-center">
+                <a href="#" class="link link--gold uppercase font-semibold ">Zobacz nasze menu </a><img src="{{asset('assets/icons/arrow-right.svg')}}" alt="" class="w-8">
+            </div>
         </div>
-    </div>
 
+        <div class=" sm:pr-20 md:pr-64 xl:pr-0 xl:col-start-7 xl:col-end-13 min-h-[500px] md:min-h-[600px] xl:-mt-[100px]  3xl:-mt-[400px]">
+          <img src="{{ asset('assets/images/7.jpeg') }}"
+          class=" rounded-lg  w-full h-full object-cover "></img>
+
+        </div>
+
+
+    </x-grid>
+</section>
+
+
+
+<section class="px-10 xl:px-20 2xl:px-40 pt-20 xl:pt-0 pb-20 xl:pb-40 bg-bgThird">
+    <x-grid>
+        <div
+            class="menu-box border border-primary-400   relative px-4  w-full sm:w-3/4 xl:w-auto mx-auto    xl:col-start-1 xl:col-end-6 xl:mx-16  flex flex-col gap-6 justify-center items-center  xl:-mt-12 py-12 xl:py-0 xl:max-h-[325px] mb-10 sm:mb-20 xl:mb-0">
+            <div class="flex flex-col justify-center items-center gap-1">
+                <span class="uppercase  text-fontPrimary font-light  font-primary">Śniadania</span>
+                <span class="uppercase  text-fontPrimary  font-light font-text">pon - nd <span
+                        class="font-semibold">07:30 - 10:00</span></span>
+            </div>
+            <div class="flex flex-col justify-center items-center gap-1">
+                <span class="uppercase  text-fontPrimary font-light font-primary">Restauracja</span>
+                <span class="uppercase  text-fontPrimary  font-light file:font-text">pon - nd <span
+                        class="font-semibold">12:00 - 23:00</span></span>
+            </div>
+            <div class="flex flex-col justify-center items-center gap-1">
+                <span class="uppercase  text-fontPrimary font-light font-primary">Kolacje Degustacyjne</span>
+                <span class="uppercase  text-fontPrimary font-light font-text">pt - sb <span class="font-semibold">17:00
+                        - 20:00</span></span>
+            </div>
+
+        </div>
+
+        <div class="xl:col-start-7 xl:col-end-13 xl:mt-32  flex flex-col gap-4 ">
+
+            <p class="font-headingSecondary  text-fontPrimary text-xl md:text-2xl xl:text-3xl  leading-snug tracking-wide">
+                "Podczas naszego pobytu w Grand Hotelu mieliśmy przyjemność zjeść kolację w hotelowej restauracji, i
+                muszę przyznać, że było to jedno z najbardziej pamiętnych doświadczeń kulinar­nych, jakie kiedykolwiek
+                mieliśmy. Danie za daniem zachwycało nas nie tylko wyjątkowym połączeniem smaków, ale także artystycznym
+                sposobem podania. Szef kuchni zdecydowanie wie, jak łączyć lokalne produkty z innowacyjnymi technikami,
+                tworząc niesamowite kompozycje, które są prawdziwym uczciem dla oczu i podniebienia."
+            </p>
+            <div class="flex gap-6">
+
+                <span class="uppercase text-primary-400 font-light">Marian, google / 21-08-2023</span>
+                <div class="flex " style="margin-top:-3px ">
+                  <img src="{{ asset('assets/icons/star.svg') }}" alt="" class="w-5">
+                  <img src="{{ asset('assets/icons/star.svg') }}" alt="" class="w-5">
+                  <img src="{{ asset('assets/icons/star.svg') }}" alt="" class="w-5">
+                  <img src="{{ asset('assets/icons/star.svg') }}" alt="" class="w-5">
+                  <img src="{{ asset('assets/icons/star.svg') }}" alt="" class="w-5">
+                
+                </div>
+            </div>
+        </div>
+    </x-grid>
 </section>
